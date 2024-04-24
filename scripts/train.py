49879964fbb49f9cc3b2a6fe04011813b3338ccb
Georgiy Sarapulov <sarapulov@biocad.ru>
2024-04-24 18:10:43 +0300

    training script draft

---
+++ scripts/train.py
@@ -0,0 +1,29 @@
+import sys
+from pathlib import Path
+
+import lightning as L
+import torch
+from torch import nn
+
+sys.path.append(str(Path.cwd()))
+
+from rpp_dock.data.datamodule import DataModule
+from rpp_dock.model.fake_model import DummyDenoisingModel
+from rpp_dock.model.lightning import Denoiser
+
+if __name__ == "__main__":
+    datamodule = DataModule(
+        pdbdir="structures/",
+        train_csv="splits_test.csv",
+        val_csv="splits_test.csv",
+        batch_size=2,
+        transform_steps=10,
+    )
+    datamodule.setup("fit")
+    batch = next(iter(datamodule.train_dataloader()))
+    model = Denoiser(
+        model=DummyDenoisingModel(),
+        optimizer_fn=lambda params: torch.optim.Adam(params, lr=0.001),
+    )
+    trainer = L.Trainer(max_epochs=10, logger=False, accelerator="cpu")
+    trainer.fit(model, datamodule)
